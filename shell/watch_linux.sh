b19d865cebbb1df8a1f8176810fcfdbf70f166d1
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2025-03-23 12:48:48 +0100

    Update tectonic build config

--- shell/watch_linux.sh
+++
@@ -1,7 +0,0 @@
-echo "Building document"
-tectonic -X watch --exec "build --keep-intermediates"
-
-echo "Viewing document"
-nohup xdg-open "$HOME/Physik/Promotion/thesis/build/default/default.pdf" &> /dev/null &
-# Need to sleep a tiny amount of time so that evince has time to start before the shell exits
-sleep 1e-3
