965ebfc95a05be47c694a960c1aebe6746ad0993
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2025-04-01 19:07:01 +0200

    Update run configs

--- shell/watch_windows.ps1
+++ shell/watch_windows.ps1
@@ -1,10 +1,13 @@
+$Env:MAMBA_EXE="$Env:LOCALAPPDATA\miniforge3\Library\bin\mamba.exe"
+$Env:MAMBA_ROOT_PREFIX="$Env:LOCALAPPDATA\miniforge3"
+
 Write-Host "Activating environment"
 # We run without a profile to keep shell startup time at a minimum. The conda hook still needs to be invoked.
 (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 mamba activate tectonic
 
 Write-Host "Viewing document"
-Start-Process $env:localappdata\SumatraPDF\sumatrapdf.exe -ArgumentList @("-reuse-instance", "build\default\default.pdf")
+Start-Process $Env:LOCALAPPDATA\SumatraPDF\sumatrapdf.exe -ArgumentList @("-reuse-instance", "build\default\default.pdf")
 
 Write-Host "Building document"
 & "tectonic.exe" -X watch --exec "build --keep-intermediates"
