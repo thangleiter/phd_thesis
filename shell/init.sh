f19cadcb3f4dc3092ec2a66d8a2e407beeb8882e
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2025-04-25 08:40:19 +0200

    Add init.sh for nix setup

---
+++ shell/init.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+echo "Installing git hooks."
+git config core.hooksPath .githooks
+echo "Initializing git submodules."
+git submodule update --init
+echo "Running post-checkout hook"
+if ! "$(git rev-parse --show-toplevel)"/.githooks/post-checkout; then
+    echo "Something went wrong."
+    exit 1
+fi
+
+echo "Success!"
+echo "Compile with 'latexmk -r linux.latexmkrc -pv'"
+
+exit 0
+
