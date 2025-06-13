27caae564bdc6e34037a79a762d22e554a55eb87
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2025-06-13 14:50:39 +0200

    Some work

--- img/py/setup/g2.py
+++ img/py/setup/g2.py
@@ -10,7 +10,7 @@ import xarray as xr
 from mjolnir.helpers import save_to_hdf5
 from mjolnir.plotting import plot_nd  # noqa
 from qcodes.dataset import initialise_or_create_database_at
-from qutil import const
+from qutil import const, plotting
 from qutil.plotting.colors import RWTH_COLORS
 from uncertainties import ufloat
 
@@ -36,6 +36,10 @@ def g2_model(tau, gamma, g0):
     return np.square(g0*(1 - np.exp(-0.5*gamma*np.abs(tau))))
 
 
+def lorentz(E, E_0, gamma, A):
+    return A * gamma / (gamma**2 + (E - E_0)**2)
+
+
 # %% Extract runs from database
 if EXTRACT_DATA:
     initialise_or_create_database_at(ORIG_DATA_PATH)
@@ -46,6 +50,29 @@ if EXTRACT_DATA:
 power_dependence = xr.load_dataset(DATA_PATH / 'power_dependence.h5')
 # Plot with interactive sliders
 # fig, ax, sliders = plot_nd(power_dependence, norm=mpl.colors.AsinhNorm(vmin=0))
+
+bounds = [(1.4182, 1.4187), (1.4194, 1.4198), (1.4210, 1.4214)]
+fits = []
+masks = []
+powers = []
+for bound in bounds:
+    mask = ((power_dependence.ccd_horizontal_axis > bound[0])
+            & (power_dependence.ccd_horizontal_axis < bound[1]))
+    fit = power_dependence.sel(ccd_horizontal_axis=mask).curvefit(
+        'ccd_horizontal_axis',
+        lorentz,
+        p0={'E_0': np.mean(bound), 'gamma': np.diff(bound), 'A': 1},
+        bounds={'E_0': bound, 'gamma': (0, 1e-3), 'A': (0, np.inf)}
+    )
+    # power = lorentz(
+    #     power_dependence.sel(ccd_horizontal_axis=mask),
+    #     *fit.ccd_ccd_data_rate_bg_corrected_curvefit_coefficients.T
+    # ).integrate('ccd_horizontal_axis')
+    power = power_dependence.sel(ccd_horizontal_axis=mask).integrate('ccd_horizontal_axis')
+
+    fits.append(fit)
+    masks.append(mask)
+    powers.append(power)
 
 # %%% Plot
 power_data = power_dependence.ccd_ccd_data_rate_bg_corrected.sel(
@@ -63,24 +90,47 @@ ax2.set_xticks([868, 871, 874, 877])
 
 fig.savefig(SAVE_PATH / 'ingaas_pl.pdf')
 
+# %%% Plot fits, not used
+with plotting.changed_plotting_backend('qtagg'):
+    fig, axs = plt.subplots(2, layout='constrained', sharex=True, figsize=(MARGINWIDTH, 2.25))
+    axs[0].semilogx(
+        power_dependence.excitation_path_power_at_sample * 1e9,
+        np.transpose([fit.ccd_ccd_data_rate_bg_corrected_curvefit_coefficients[:, 1] * 1e6
+                      for fit in fits])
+    )
+    axs[1].loglog(
+        power_dependence.excitation_path_power_at_sample * 1e9,
+        np.transpose([power.ccd_ccd_data_rate_bg_corrected for power in powers]) * const.e * 1e24
+    )
+    axs[0].grid()
+    axs[1].grid()
+
+    axs[1].set_yticks([1, 10, 100, 1_000, 10_000])
+
+    axs[1].set_xlabel('$P$ (nW)')
+    axs[1].set_ylabel('$P$ (yW)')
+    axs[0].set_ylabel(r'$\gamma$ (μeV)')
 # %% G2 measurement
 g2 = xr.load_dataset(DATA_PATH / 'g2.h5')
 
 g2_data = g2.tagger_correlation_1_data_normalized
 g2_data_log = g2.tagger_histogram_log_bins_1_g2
 
-mask = np.abs(x := g2_data.tagger_correlation_1_time_bins) < 12e3
+mask = np.abs(x := g2_data.tagger_correlation_1_time_bins) < 11e3
 popt, pcov = sc.optimize.curve_fit(g2_model, x[mask], g2_data.data.squeeze()[mask], p0=[1e-3, 1],
                                    bounds=list(zip((0, np.inf), (0, 1))))
 
 τmax = g2_data_log.tagger_histogram_log_bins_1_time_bins[g2_data_log.argmax()].item()
 print(f'γ = {ufloat(popt[0], np.sqrt(np.diag(pcov))[0])} THz')
+print(f'g0 = {ufloat(popt[1], np.sqrt(np.diag(pcov))[1])}')
 print(f'Bump is at τ = {τmax} ps = {τmax*1e-12*const.c} m')
 # %%% Plot
 DOWNSAMPLING = 10
+x_down = x[DOWNSAMPLING//2::DOWNSAMPLING]
+y_down = np.reshape(g2_data.data, (-1, DOWNSAMPLING)).mean(-1)
 
-fig, axs = plt.subplots(2, layout='constrained', sharey=True, figsize=(MARGINWIDTH, 2.25))
-axs[0].plot(x[::DOWNSAMPLING] * 1e-3, g2_data.T[::DOWNSAMPLING],
+fig, axs = plt.subplots(2, layout='constrained', sharey=False, figsize=(MARGINWIDTH, 2.25))
+axs[0].plot(x_down * 1e-3, y_down,
             **markerprops(RWTH_COLORS['blue'], marker='.', markeredgealpha=0.75,
                           markerfacealpha=0.25))
 axs[0].plot(x*1e-3, g2_model(x, *popt))
@@ -90,10 +140,10 @@ axs[1].semilogx(g2_data_log.tagger_histogram_log_bins_1_time_bins * 1e-3, g2_dat
 
 axs[0].grid()
 axs[1].grid()
-axs[0].set_yticks([0, 1, 2])
 axs[1].set_yticks([0, 1, 2])
-axs[0].set_xlim(-12, 12)
-axs[0].set_ylim(top=2)
+axs[0].set_xlim(-11, 11)
+axs[0].set_ylim(top=1.3)
+axs[1].set_ylim(top=2)
 axs[1].set_xlabel(r'$\tau$ (ns)')
 fig.supylabel(r'$g^{(2)}(\tau)$', fontsize=10)
 
